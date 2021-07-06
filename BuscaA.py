0f7fb4cfed7fcefbefd83134b9860d01c191b4ba
mateusdoimo <mateusdoimo@usp.br>
2021-07-06 19:20:47 -0300

    busca a

--- BuscaA.py
+++ BuscaA.py
@@ -1,2 +1,16 @@
+import math
+
 class BuscaA():
-    pass
+
+    def heuristica(self, grafo, objetivo):
+        for vertice in grafo.lista_Vertices:
+            dx = abs(objetivo.getX() - vertice.getX())
+            dy = abs(objetivo.getY() - vertice.getY())
+            vertice.setHeuristica(math.sqrt(pow(dx, 2) + pow(dy, 2)))
+
+    def start(self, grafo, inicio, fim):
+        f = [ 0 for i in range(len(grafo)) ]
+        g = [ 0 for i in range(len(grafo)) ]
+
+        objetivo = grafo.busca_Vertice(fim)
+        self.heuristica(grafo, objetivo)
